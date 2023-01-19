bd30a4fdaf92613edefcf17bdabc682833aa75ca
Andrey Kharginen <kut13@inbox.ru>
2023-01-19 14:06:25 +0400

    corrected SQRService

--- src/main/java/ru/netology/sqr/main/SQRService.java
+++ src/main/java/ru/netology/sqr/main/SQRService.java
@@ -1,10 +1,11 @@
 package ru.netology.sqr.main;
 public class SQRService {
     public int calcSQRt(int x, int y) {
+        int counter = 0;
         for (int i = 10; i < 100; i++) {
             if ((i * i >= x) & (i * i <= y))
-                return i ;
+               counter = counter + 1;
         }
-        return -1;
+        return counter;
     }
 }
