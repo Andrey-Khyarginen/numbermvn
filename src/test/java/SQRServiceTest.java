940fdb965157c15e06796fd8461cf1cc4b266b44
Andrey Kharginen <kut13@inbox.ru>
2023-01-19 17:34:50 +0400

    corrected SQRServicTest

--- src/test/java/SQRServiceTest.java
+++ src/test/java/SQRServiceTest.java
@@ -1,22 +1,24 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.netology.sqr.main.SQRService;
+
 public class SQRServiceTest {
     @Test
     public void testFirstNumberRange() {
         SQRService service = new SQRService();
 
-        int expected = 15;
+        int expected = 3;
         int actual = service.calcSQRt(200, 300);
 
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     public void testSecondNumberRange() {
         SQRService service = new SQRService();
 
-        int expected = 16;
-        int actual = service.calcSQRt(200, 300);
+        int expected = 8;
+        int actual = service.calcSQRt(200, 500);
 
         Assertions.assertEquals(expected, actual);
     }
